5e2025260c166c808b8a376ec63e86462ee506b5
tomus1857 <tomus1857.2001@o2.pl>
2025-08-28 17:50:15 +0200

    Added session to postgresql with marten and started postgresql as a container

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -18,7 +18,6 @@
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Create Product")
             .WithDescription("Create Product");
-            // chapter 6 odcinek 1
         }
     }
 }
