fa15b1a64436e30906d7dfd6240ce5ecd793d90a
tomus1857 <tomus1857.2001@o2.pl>
2025-09-18 17:44:31 +0200

    Developing Basket microservices

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Basket.API.Models;
+using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.GetBasket
+{
+    //public record GetBasketRequest(string UserName);
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var query = new GetBasketQuery(userName);
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetBasketResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket by UserName");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO : get basket from database
+            // var basket = await _repository.GetBasket(query.UserName);
+            return new GetBasketResult(new ShoppingCart(query.UserName));
+        }
+    }
+}
